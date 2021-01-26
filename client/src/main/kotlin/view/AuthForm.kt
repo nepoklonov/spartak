08cec2ae79a910653ebf1890a4c22e434b459eb0
nepoklonov <i.s.nepoklonov@yandex.ru>
2021-01-26 12:35:52 +0300

    form fixed

--- client/src/main/kotlin/view/AuthForm.kt
+++ client/src/main/kotlin/view/AuthForm.kt
@@ -1,25 +1,15 @@
 package view
 
-import kotlinx.coroutines.CoroutineScope
-
 import kotlinx.html.InputType
 import kotlinx.html.js.onChangeFunction
 import kotlinx.html.js.onSubmitFunction
-import model.Check
 import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.events.Event
 import react.*
-import react.dom.br
-import react.dom.input
-import services.CheckService
-import styled.StyledDOMBuilder
-import styled.styledDiv
 import styled.styledForm
 import styled.styledInput
 
-
-external interface AuthFormProps : RProps {
-}
+external interface AuthFormProps : RProps
 
 class AuthFormState : RState {
     var inputs: MutableMap<String, String> = mutableMapOf("login" to "", "password" to "")
@@ -29,32 +19,31 @@ class AuthFormComponent : RComponent<AuthFormProps, AuthFormState>() {
     init {
         state = AuthFormState()
     }
-    override fun componentDidMount() {
-    }
-    private fun handleSubmit (event: Event){
-        event.preventDefault()
-        event.stopPropagation()
-        console.log("Login : ${this.state.inputs["login"]} password : ${this.state.inputs["password"]}")
 
+    private fun handleSubmit(event: Event) {
+        event.preventDefault()
+        console.log("Login : ${state.inputs["login"]} password : ${state.inputs["password"]}")
     }
 
     private fun handleChange(event: Event) {
-        console.log(event.target)
         val target = event.target as HTMLInputElement
-        setState{
-            this.inputs[target.name] = target.value
+        setState {
+            inputs[target.name] = target.value
         }
     }
 
     override fun RBuilder.render() {
         styledForm {
+            attrs.onSubmitFunction = {
+                handleSubmit(it)
+            }
             styledInput(type = InputType.text) {
                 attrs.onChangeFunction = {
                     handleChange(it)
                 }
                 attrs {
-                    this.name = "login"
-                    this.value = state.inputs["login"]!!
+                    name = "login"
+                    value = state.inputs["login"]!!
                 }
             }
             styledInput(type = InputType.password) {
@@ -63,14 +52,11 @@ class AuthFormComponent : RComponent<AuthFormProps, AuthFormState>() {
                 }
 
                 attrs {
-                    this.name = "password"
-                    this.value = state.inputs["password"]!!
+                    name = "password"
+                    value = state.inputs["password"]!!
                 }
             }
             styledInput {
-                attrs.onSubmitFunction = {
-                    handleSubmit(it)
-                }
                 attrs.type = InputType.submit
                 attrs.value = "Submit"
             }
