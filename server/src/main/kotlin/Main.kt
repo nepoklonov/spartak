99b55df4053b51bd03b7e47e63b5b9d19d621b9b
uhTVik <uhatov007@yandex.ru>
2021-02-14 04:48:49 +0300

    Upload file tried

--- server/src/main/kotlin/Main.kt
+++ server/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+
 import database.*
 import io.ktor.application.*
 import io.ktor.features.*
@@ -5,10 +7,11 @@ import io.ktor.html.*
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.jackson.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.sessions.*
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlinx.css.*
 import kotlinx.css.properties.lh
 import kotlinx.html.*
@@ -16,7 +19,9 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.insert
 import rpc.rpc
 import services.*
-
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
 
 
 private val globalCss = CSSBuilder().apply {
@@ -309,6 +314,7 @@ fun Application.main() {
         static("newsHtml") { files("newsHtml") }
 
 
+
         route("/api") {
             rpc(CheckService::class)
             rpc(GameService::class)
@@ -326,6 +332,49 @@ fun Application.main() {
             rpc(GalleryNavigationService::class)
             rpc(GamesNavigationService::class)
             rpc(WorkoutsNavigationService::class)
+
+            post("upload/image") {
+                suspend fun InputStream.copyToSuspend(
+                    out: OutputStream,
+                    bufferSize: Int = DEFAULT_BUFFER_SIZE,
+                    yieldSize: Int = 4 * 1024 * 1024,
+                    dispatcher: CoroutineDispatcher = Dispatchers.IO
+                ): Long {
+                    return withContext(dispatcher) {
+                        val buffer = ByteArray(bufferSize)
+                        var bytesCopied = 0L
+                        var bytesAfterYield = 0L
+                        while (true) {
+                            val bytes = read(buffer).takeIf { it >= 0 } ?: break
+                            out.write(buffer, 0, bytes)
+                            if (bytesAfterYield >= yieldSize) {
+                                yield()
+                                bytesAfterYield %= yieldSize
+                            }
+                            bytesCopied += bytes
+                            bytesAfterYield += bytes
+                        }
+                        return@withContext bytesCopied
+                    }
+                }
+
+                val multipart = call.receiveMultipart()
+                multipart.forEachPart { part ->
+                    when (part) {
+                        is PartData.FileItem -> {
+                            val uploadDir = "uploads"  // CREATED LINE
+                            val ext = File(part.originalFileName).extension
+                            val file = File(uploadDir, "upload-${System.currentTimeMillis()}.$ext")
+                            part.streamProvider().use { input ->
+                                file.outputStream().buffered().use { output -> input.copyToSuspend(output) }
+                            }
+                        }
+                    }
+
+                    part.dispose()
+                }
+            }
         }
     }
 }
+
